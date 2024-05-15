72b594a1c534403e6fb5e0d8f0b3e687c31852ea
sachin <sachinsharma21682gmail.com>
2024-05-15 23:48:36 +0530

    Initial Commit

---
+++ swiftSafari/swiftSafariApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftSafariApp.swift
+//  swiftSafari
+//
+//  Created by Sachin Sharma on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftSafariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
